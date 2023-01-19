e17e695b9ba57cca7c78f0f5704ca299e189ab47
Claudio Traspadini Oliveira <claudio@autonomic.com>
2023-01-19 10:50:44 -0700

    Created cross platform build

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-xk6 build --with xk6-fabric=. --with github.com/mostafa/xk6-kafka 
+#Mac OS
+xk6 build --with xk6-fabric=. --with github.com/mostafa/xk6-kafka --output k6-darwin
+#Linux
+GOOS=linux GOARCH=amd64 xk6 build --with xk6-fabric=. --with github.com/mostafa/xk6-kafka --output k6-linux
